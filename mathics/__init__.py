041683ff5883d5004a7853d7f1b057fdfa2e8e63
Angus Griffith <16sn6uv@gmail.com>
2013-07-30 10:33:13 +1000

    cleanup version/copyright banner

--- mathics/__init__.py
+++ mathics/__init__.py
@@ -26,26 +26,35 @@ def get_version():
     version['python'] = sys.subversion[0] + " " + sys.version.split('\n')[0]
     return version
 
-def get_version_string(is_server, newlines=False):
+def get_version_string(is_server, newlines=False, verbose=True):
     version = get_version()
     result = []
     result.append(u"Mathics %s" % version['mathics'])
-    result.append(u"on %s" % version['python'])
-    libs = []
-    if 'django' in version and is_server:
-        libs.append("Django %s" % version['django'])
-    libs += ["SymPy %s" % version['sympy'], "mpmath %s" % version['mpmath']]
-    result.append(u"using %s" % ", ".join(libs))
+    if verbose:
+        result.append(u"on %s" % version['python'])
+        libs = []
+        if 'django' in version and is_server:
+            libs.append("Django %s" % version['django'])
+        libs += ["SymPy %s" % version['sympy'], "mpmath %s" % version['mpmath']]
+        result.append(u"using %s" % ", ".join(libs))
+    return ("\n" if newlines else " ").join(result)
+
+def get_license_string(newlines=False, verbose=True):
+    result = ["Copyright (C) 2011-2013 The Mathics Team."]
+    if verbose:
+        result += [
+            "This program comes with ABSOLUTELY NO WARRANTY.",
+            "This is free software, and you are welcome to redistribute it",
+            "under certain conditions.",
+            "See the documentation for the full license."]
     return ("\n" if newlines else " ").join(result)
 
 def print_version(is_server):
     print "\n" + get_version_string(is_server, newlines=True)
-    
+
 def print_license():
-    print u"""
-Copyright (C) 2011-2013 The Mathics Team.
-This program comes with ABSOLUTELY NO WARRANTY.
-This is free software, and you are welcome to redistribute it
-under certain conditions.
-See the documentation for the full license.
-"""
+    print "\n" + get_license_string(newlines=True)
+
+def get_banner_string(is_server, verbose=True):
+    return (get_version_string(is_server, newlines=True, verbose=verbose) + "\n"
+            + get_license_string(newlines=True, verbose=verbose) + "\n")

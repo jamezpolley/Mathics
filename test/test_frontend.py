9e650089752e7f223afa068468b77e4b1cfd277d
Angus Griffith <16sn6uv@gmail.com>
2013-09-28 15:50:25 +1000

    more web frontend tests

--- test/test_frontend.py
+++ test/test_frontend.py
@@ -26,7 +26,7 @@ class FrontendTest():
         time.sleep(1)
 
     def tearDown(self):
-        self.driver.close()
+        self.driver.quit()
         self.server.terminate()
 
     def test_basic(self):
@@ -45,16 +45,96 @@ class FrontendTest():
             "Mathics uses MathJax to display beautiful math.\n"
             "Visit mathics.org for further information about the project.")
 
-    def test_documentation(self):
+    def test_doc_links(self):
         driver = self.driver
 
+        doclink = driver.find_element_by_id("doclink")
+        self.assertEqual(doclink.text, "Documentation")
+        doclink.click()
+        time.sleep(0.1)
+
+        doc = driver.find_element_by_id('docContent')
+        self.assertTrue(doc.is_displayed())
+        self.assertTrue(
+            doc.text.startswith("Documentation\nManual\nIntroduction\n"))
+
+        doc.find_element_by_partial_link_text("Introduction").click()
+        time.sleep(0.1)
+        doc = driver.find_element_by_id('docContent')
+        self.assertEqual(
+            doc.find_element_by_tag_name("h1").text, "Introduction")
+        self.assertTrue(
+            doc.find_element_by_tag_name("p").text.startswith("Mathics"))
+
+        doc.find_element_by_partial_link_text("Installation").click()
+        time.sleep(0.1)
+        doc = driver.find_element_by_id('docContent')
+        self.assertEqual(
+            doc.find_element_by_tag_name("h1").text, "Installation")
+
+        doc.find_element_by_partial_link_text("Overview").click()
+        time.sleep(0.1)
+        doc = driver.find_element_by_id('docContent')
+        self.assertEqual(
+            doc.find_element_by_tag_name("h1").text, "Documentation")
+        doclink.click()
+        time.sleep(0.1)
+        doc = driver.find_element_by_id('docContent')
+        self.assertFalse(doc.is_displayed())
+
+    def test_doc_search(self):
+        driver = self.driver
         search = driver.find_element_by_id('search')
+        self.assertEqual(search.text, "")
+
         for key in "Plus":
             search.send_keys(key)
             time.sleep(0.2)
 
         doc = driver.find_element_by_id('docContent')
         self.assertEquals(doc.find_element_by_tag_name('h1').text, 'Plus (+)')
+
+    def test_keyboard_commands(self):
+        driver = self.driver
+        body = driver.find_element_by_tag_name("body")
+
+        # Ctrl-D
+        body.send_keys(Keys.CONTROL + "D")
+        docsearch = driver.switch_to_active_element()
+        docsearch.send_keys("D")
+        time.sleep(0.5)
+        doc = driver.find_element_by_id('docContent')
+        self.assertTrue(doc.is_displayed())
+        self.assertEqual(
+            doc.find_element_by_tag_name("h1").text, "D")
+
+        # Ctrl-C
+        body.send_keys(Keys.CONTROL + "C")
+        time.sleep(0.5)
+        self.assertTrue(doc.is_displayed())
+        focus = driver.switch_to_active_element()
+        self.assertEqual(focus.tag_name, "textarea")
+        self.assertEqual(focus.text, "")
+
+        # Ctrl-O
+        popup = driver.find_element_by_id("open")
+        self.assertFalse(popup.is_displayed())
+        body.send_keys(Keys.CONTROL + "O")
+        time.sleep(0.5)
+        self.assertTrue(popup.is_displayed())
+        popup.find_element_by_class_name("cancel").click()
+        time.sleep(0.5)
+        self.assertFalse(popup.is_displayed())
+
+        # Ctrl-S
+        popup = driver.find_element_by_id("save")
+        self.assertFalse(popup.is_displayed())
+        body.send_keys(Keys.CONTROL + "S")
+        time.sleep(0.5)
+        self.assertTrue(popup.is_displayed())
+        popup.find_element_by_class_name("cancel").click()
+        time.sleep(0.5)
+        self.assertFalse(popup.is_displayed())
 
     def test_query(self):
         driver = self.driver
@@ -80,20 +160,20 @@ class FrontendTest():
 
 def has_driver(driver):
     try:
-        driver().close()
+        driver().quit()
     except WebDriverException:
         return False
     else:
         return True
 
+# Tests fail under chrome webdriver - can't focus on correct element
+# @unittest.skipUnless(has_driver(webdriver.Chrome), "no Chrome")
+# class ChromeTest(FrontendTest, unittest.TestCase):
+#     pass
+
 
 @unittest.skipUnless(has_driver(webdriver.Firefox), "no Firefox")
 class FirefoxTest(FrontendTest, unittest.TestCase):
-    pass
-
-
-@unittest.skipUnless(has_driver(webdriver.Chrome), "no Chrome")
-class ChromeTest(FrontendTest, unittest.TestCase):
     pass
 
 

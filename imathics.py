367f31475e866fd47895a9e67296240574b831cb
Angus Griffith <16sn6uv@gmail.com>
2014-01-11 17:30:39 +1100

    refactor imathics.py

--- imathics.py
+++ imathics.py
@@ -3,57 +3,54 @@ import os
 import sys
 import json
 import zmq
+import time
 from threading import Thread
 
 from IPython.utils.localinterfaces import LOCALHOST
 from IPython.kernel.zmq.session import Session
+
 
 class Heartbeat(Thread):
     "A simple ping-pong style heartbeat that runs in a thread."
     def __init__(self, context, config):
         Thread.__init__(self)
         self.context = context
-        self.ip = config['ip']
-        self.port = config['hb_port']
-
-        # if self.port == 0:
-        #     s = socket.socket()
-        #     # '*' means all interfaces to 0MQ, which is '' to socket.socket
-        #     s.bind(('' if self.ip == '*' else self.ip, 0))
-        #     self.port = s.getsockname()[1]
-        #     s.close()
-        self.addr = (self.ip, self.port)
+        self.config = config
         self.daemon = True
+        self.socket = self.context.socket(zmq.REP)
 
     def run(self):
-        self.socket = self.context.socket(zmq.REP)
-        self.socket.bind('tcp://%s:%i' % self.addr)
+        self.socket.bind(
+            "{transport}://{ip}:{hb_port}".format(**self.config))
         zmq.device(zmq.FORWARDER, self.socket, self.socket)
 
 
 class Kernel(object):
     def __init__(self, context, config):
         self.context = context
+        self.config = config
+        self.session = Session(username='MathicsKernel', debug=False)
         self.stdin = self.context.socket(zmq.ROUTER)
         self.shell = self.context.socket(zmq.ROUTER)
         self.iopub = self.context.socket(zmq.PUB)
 
-        self.session = Session(debug=True)
-
-        self.stdin.bind("{transport}://{ip}:{stdin_port}".format(**config))
-        self.shell.bind("{transport}://{ip}:{shell_port}".format(**config))
-        self.iopub.bind("{transport}://{ip}:{iopub_port}".format(**config))
-
     def start(self):
+        self.stdin.bind(
+            "{transport}://{ip}:{stdin_port}".format(**self.config))
+        self.shell.bind(
+            "{transport}://{ip}:{shell_port}".format(**self.config))
+        self.iopub.bind(
+            "{transport}://{ip}:{iopub_port}".format(**self.config))
         # start kernel
         self.session.send(self.iopub, 'status',
-                     content={'execution_state': 'starting'})
+                          content={'execution_state': 'starting'})
 
         # TODO Start the mathics kernel
 
         self.session.send(self.iopub, 'status',
-                     content={'execution_state': 'idle'})
+                          content={'execution_state': 'idle'})
 
+        # Event Loop
         while True:
             shell_idents, shell_msg = self.session.recv(self.shell)
 
@@ -61,45 +58,54 @@ class Kernel(object):
                 continue
 
             msg_type = shell_msg['msg_type']
-            if msg_type == 'kernel_info_request':
-                self.session.send(self.shell, 'kernel_info_reply', content={
-                    'protocol_version': [1, 1, 0],
-                    'language_version': [0, 6],
-                    'language': 'mathics',
-                })
-            elif msg_type == 'execute_request':
-                request_content = shell_msg['content']
+            handler = getattr(self, shell_msg['msg_type'], None)
+            if handler is None:
+                raise ValueError("Unknown msg_type %s" % shell_msg['msg_type'])
+            handler(shell_msg)
 
-                # report the kernel as busy
-                self.session.send(self.iopub, 'status',
-                                  content={'execution_state': 'busy'})
+    def kernel_info_request(self, shell_msg):
+        self.session.send(
+            self.shell, 'kernel_info_reply',
+            parent=shell_msg['header'], content={
+                'protocol_version': [1, 1, 0],
+                'language_version': [0, 6],
+                'language': 'mathics'})
 
-                self.session.send(self.iopub, 'pyin', content={
-                    'code': request_content['code'],
-                    'execution_count': 1,
-                })
+    def execute_request(self, shell_msg):
+        request_content = shell_msg['content']
 
-                self.session.send(self.iopub, 'pyout', content={
-                    'execution_count': 1,
-                    'data': {'text/plain': '123'},
-                    'metadata': {},
-                })
+        # report the kernel as busy
+        self.session.send(
+            self.iopub, 'status', content={'execution_state': 'busy'})
 
-                # sucessful execution
-                self.session.send(self.shell, 'execute_reply', content={
-                    'status': 'ok',
-                    'execution_count': 1,
-                    'payload': [],
-                    'user_variables': {},
-                    'user_expressions': {},
-                })
+        # self.session.send(
+        #     self.iopub, 'pyin', content={
+        #         'code': request_content['code'],
+        #         'execution_count': 1})
 
+        # evaluate the code
+        result_text = "eval(" + request_content['code'] + ")"
 
-                # report the kernel as idle
-                self.session.send(self.iopub, 'status',
-                             content={'execution_state': 'idle'})
-            else:
-                raise ValueError("Unknown msg_type %s" % msg_type)
+        self.session.send(
+            self.iopub, 'pyout', content={
+                'execution_count': 1,
+                'data': {'text/plain': result_text},
+                'metadata': {}})
+
+        # sucessful execution
+        self.session.send(
+            self.shell, 'execute_reply', parent=shell_msg['header'],
+            content={
+                'status': 'ok',
+                'execution_count': 1,
+                'payload': [],
+                'user_variables': {},
+                'user_expressions': {}})
+
+        # report the kernel as idle
+        self.session.send(self.iopub, 'status',
+                          content={'execution_state': 'idle'})
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
